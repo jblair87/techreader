8621d4db14b5eac61803b79292a0dfdc48b1ca21
Jenna Mandel <jenna.b.mandel@gmail.com>
2017-08-21 02:26:16 +0000

    changed path in bin files, scraped tech sites for apple news

--- lib/tech_reader/cli.rb
+++ lib/tech_reader/cli.rb
@@ -1,2 +1,30 @@
 class TechReader::CLI
+  def call
+    list_news
+    menu
+  end
+  def list_news
+      puts "Today's Apple News"
+      @news = TechReader::News.today
+      @news.each.with_index(1) do |news, i|
+        puts "#{i}. #{news.title} - #{news.author}"
+      end
+    end
+
+    def menu
+      input = nil
+      while input != "exit"
+        puts "type list to see the deals again or type exit:"
+        input = gets.strip.downcase
+
+        if input.to_i > 0
+          the_news = @news[input.to_i-1]
+          puts "#{the_news.title} - #{the_news.author}"
+        elsif input == "list"
+          list_news
+        else
+          puts "Not sure what you want, type list or exit."
+        end
+      end
+    end
 end
--- lib/tech_reader/news.rb
+++ lib/tech_reader/news.rb
@@ -16,9 +16,9 @@ def self.scrape_news
 def scrape_cnet
    doc = Nokogiri::HTML(open("https://www.cnet.com/apple/"))
    news = self.new
-   news.title =
-   news.author =
-   news.url =
+   news.title = doc.search("h2.assetBody").text
+   news.author = doc.search("#topicListing span.assetAuthor").text
+   news.url = doc.search("a").first.attr("href").strip
    news
  end
 
@@ -26,18 +26,18 @@ def scrape_cnet
  def self.scrape_verge
     doc = Nokogiri::HTML(open("http://www.theverge.com/apple"))
     news = self.new
-    news.title =
-    news.author =
-    news.url =
+    news.title = doc.search("h2.c-entry-box--compact__title").text
+    news.author = doc.search("span.c-byline__item").text
+    news.url = doc.search("a").first.attr("href").strip
     news
   end
 
   def self.scrape_recode
     doc = Nokogiri::HTML(open("https://www.recode.net/apple"))
     news = self.new
-    news.title =
-    news.author =
-    news.url =
+    news.title = doc.search("h2.c-entry-box__title").text
+    news.author = doc.search("span.c-byline__item").text
+    news.url = doc.search("a").first.attr("href").strip
     news
   end
 end
